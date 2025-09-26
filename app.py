c566bf95f0002fdea595f6a76f02637ca1a0b083
Noor Fatima <noortechdata@gmail.com>
2025-09-26 08:38:29 -0700

    app.py

--- app.py
+++ app.py
@@ -31,7 +31,11 @@ st.markdown(
 col1, col2 = st.columns([3, 1], gap="small")  # flexible columns
 
 with col1:
-    st.image(os.path.join("logo.png"), use_column_width=True)
+   st.image(
+        os.path.join("logo.png"),
+        width=120,                 # ✅ fixed width for logo
+        use_container_width=False  # ✅ prevents stretching
+    )
 
 with col2:
     with open("skills_data.csv", "rb") as csv_file:
@@ -121,4 +125,5 @@ table_html_demand = """
 </table>
 """
 st.markdown(table_html_demand, unsafe_allow_html=True)
+
 
