06f2cdae67498c239afbd0767005a1435de74593
Noor Fatima <noortechdata@gmail.com>
2025-09-26 10:38:29 -0700

    app.py

--- app.py
+++ app.py
@@ -46,15 +46,15 @@ with col1:
 
 with col2:
     st.markdown(
-        """
-        <div style="text-align:right;">
-            <a href="skills_data.csv" download="skills_data.csv">
-                <button class="download-btn">Download CSV</button>
-            </a>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+    """
+    <div style="display: flex; justify-content: flex-end; align-items: center; width: 100%;">
+        <a href="skills_data.csv" download="skills_data.csv">
+            <button class="download-btn">Download CSV</button>
+        </a>
+    </div>
+    """,
+    unsafe_allow_html=True
+)
 
 
 # --- Rectangle with hero layout ---
@@ -135,6 +135,7 @@ table_html_demand = """
 </table>
 """
 st.markdown(table_html_demand, unsafe_allow_html=True)
+
 
 
 
